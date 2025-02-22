19a218f51fd522d986d098ec14c72bb1f1498b0c
Sahger <lad.sahger@gmail.com>
2025-02-22 01:27:04 -0500

    Refactor code to enable asynchronous scraping

--- src/config.py
+++ src/config.py
@@ -1,24 +1,10 @@
 import os
 
-# Web config
-URL_ACCOUNT_LOGIN = "https://www.volosports.com/login"
-URL_QUERY_TEST = "https://www.volosports.com/discover?cityName=New%20York%20Metro%20Area&view=SPORTS&subView=DAILY&programTypes%5B%5D=PICKUP&sportNames%5B%5D=Volleyball"
-URL_QUERY = "https://www.volosports.com/discover?cityName=New%20York%20Metro%20Area&view=SPORTS&subView=DAILY&programTypes%5B%5D=PICKUP&sportNames%5B%5D=Volleyball&venueIds%5B%5D=60ad2493c1e4b9002fdbb04e&venueIds%5B%5D=60ad1c7ca5287c00919ec225&venueIds%5B%5D=64c875b059de744a3615b995&venueIds%5B%5D=60ad250fabc941002fb2f58c"
-
 # Email config
-SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 EMAIL_SENDER = "volo.notification@gmail.com"
 EMAIL_RECIPIENT = os.environ.get("EMAIL_RECIPIENT")
 APP_PASSWORD = os.environ.get("APP_PASSWORD")
-
-# Volo account
-VOLO_USERNAME = os.environ.get("VOLO_USERNAME")
-VOLO_PASSWORD = os.environ.get("VOLO_PASSWORD")
-
-# File config
-FILEPATH_LOG = "log/log_{date}.txt"
-FILEPATH_EVENT_LOG = "data/event_log.csv"
 
 # Main config
 SLEEP_TIME = 900
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import logging
 import os
-import sys
-import time
 import datetime as dt
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
-from src import (config, web_scraper, event_log, emailer)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from src import event_log, emailer, config
+from src.scrapers.volo import volo_scraper, volo_config
 
 
 def create_logger(path_log):
@@ -28,43 +32,58 @@ def create_logger(path_log):
     return logger
 
 
-def main(url, filepath):
-    driver = web_scraper.start_browser()
-    account_login = web_scraper.login_to_account(
-        driver,
-        config.URL_ACCOUNT_LOGIN,
-        config.VOLO_USERNAME,
-        config.VOLO_PASSWORD
-    )
-    existing_event_ids = event_log.read_event_ids(filepath)
-    new_events = web_scraper.get_events(driver, url, account_login, existing_event_ids)
-    if not new_events:
-        logger.info("No new open event IDs.")
-    else:
-        event_log.write_event_ids(filepath, [event["event_id"] for event in new_events])
-        emailer.send_email(**emailer.create_email_content_events(new_events))
-    driver.quit()
+def start_browser(logger, headless=True):
+    logger.info("Starting browser...")
+    chrome_options = Options()
+    if headless:
+        chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_window_size(1920, 1080)
+    logger.info("Browser started.")
+    return driver
+
+
+def main_volo(url, filepath_event):
+    logger = create_logger(volo_config.FILEPATH_LOG.format(date=dt.date.today().strftime("%Y-%m-%d")))
+    logger.info(f"Starting Volo scraper on {url}...")
+    try:
+        driver = start_browser(logger)
+        account_login = volo_scraper.login_to_account(
+            driver,
+            volo_config.URL_ACCOUNT_LOGIN,
+            volo_config.USERNAME,
+            volo_config.PASSWORD
+        )
+        existing_event_ids = event_log.read_event_ids(filepath_event)
+        new_events = volo_scraper.get_events(driver, url, account_login, existing_event_ids)
+        if not new_events:
+            logger.info("No new open event IDs.")
+        else:
+            event_log.write_event_ids(filepath_event, [event["event_id"] for event in new_events])
+            emailer.send_email(**emailer.create_email_content_events(new_events))
+        driver.quit()
+        retry_counter["volo"] = 0
+        logger.info(f"Volo webscrape completed successfully.")
+    except Exception as e:
+        retry_counter["volo"] += 1
+        logger.warning(f"Execution failed. Incrementing retry counter: {retry_counter}")
+        logger.exception(e)
+        if retry_counter == config.RETRY_LIMIT:
+            logger.fatal("Retry limit exceeded.")
+            emailer.send_email(**emailer.create_email_content_job_failure(e))
+
+
+async def main():
+    scrapers = [
+        ("volo", main_volo, volo_config.URL_QUERY, volo_config.FILEPATH_EVENT_LOG)
+    ]
+    with ThreadPoolExecutor(max_workers=len(scrapers)) as executor:
+        loop = asyncio.get_running_loop()
+        tasks = [loop.run_in_executor(executor, func, url, log) for name, func, url, log in scrapers]
+        await asyncio.gather(*tasks)
+        await asyncio.sleep(config.SLEEP_TIME)
 
 
 if __name__ == "__main__":
-    retry_counter = 0
-    url = config.URL_QUERY
-    while True:
-        logger = create_logger(config.FILEPATH_LOG.format(date=dt.date.today().strftime("%Y-%m-%d")))
-        logger.info(f"Starting the scraping process on {url}...")
-        try:
-            main(url, config.FILEPATH_EVENT_LOG)
-            retry_counter = 0
-            logger.info(f"Webscrape completed successfully. Sleeping for {config.SLEEP_TIME // 60} minute(s).")
-            time.sleep(config.SLEEP_TIME)
-        except Exception as e:
-            retry_counter += 1
-            logger.warning(f"Execution failed. Incrementing retry counter: {retry_counter}")
-            logger.exception(e)
-            if retry_counter == config.RETRY_LIMIT:
-                logger.fatal("Retry limit exceeded.")
-                emailer.send_email(**emailer.create_email_content_job_failure(e))
-                logger.fatal("Exiting the program...")
-                sys.exit(1)
-            logger.info(f"Sleeping for {(config.SLEEP_TIME_RETRY / 60):.1f} minutes")
-            time.sleep(config.SLEEP_TIME_RETRY)
+    retry_counter = {"volo": 0}
+    asyncio.run(main())
